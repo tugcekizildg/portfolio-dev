b0d2a8d1d41ed4614b6aab84fbf2212c10a3f20d
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-08-03 17:11:29 +0300

    Featured projects added and improved consistency level

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,7 @@
+import FeaturedProjects from '~/components/FeaturedProjects';
 import type { Route } from './+types/index';
+import type { Project } from '~/types';
+import { useLoaderData } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +10,21 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <>HomePage</>;
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+  return { projects: data };
 }
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  return (
+    <>
+      <FeaturedProjects projects={projects} count={2} />
+    </>
+  );
+};
+
+export default HomePage;
